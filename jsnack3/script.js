0058adf7ef415c2dd5653a1b945bd82c3b2a98fc
albertofanini-prog <albertofanini@outlook.com>
2022-02-11 17:30:18 +0100

    7th Commit - JSnack 4 - While

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,10 +1,20 @@
 //Dichiarare il dato richeisto
 let numbers = []
 
-for (i = 0; i < 5; i++){
-    let userNumber = parseInt( prompt ('Inserire un numero'));
+/*for (i = 0; i < 5; i++){
+    let userNumber = parseInt( prompt ('Inserire un numero per 5 volte.'));
     numbers.push(userNumber);
 
     let answer = document.querySelector('.numbers');
-    answer.innerHTML = `Hai inserito: ${numbers}`
+    answer.innerHTML = `Hai inserito: ${numbers}`;
+}*/
+
+while (numbers.length <=4){
+    let userNumber = parseInt ( prompt ('Inserire un numero per 5 volte.'));
+  
+    if (userNumber !== isNaN);
+    numbers.push(userNumber);
+
+    let answer = document.querySelector('.numbers');
+    answer.innerHTML = `Hai inserito: ${numbers}.`;
 }
